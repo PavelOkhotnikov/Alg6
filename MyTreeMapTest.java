ad54bd0882e45e247838e5f3bb6c79a0f4640825
Pavel <iamhere-lookatme@mail.ru>
2021-04-01 20:13:55 +0300

    Alg6

---
+++ MyTreeMapTest.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+class MyTreeMapTest {
+
+    public void homework() {
+        int countOfTrees = 20;
+
+        int balanced = 0;
+        for (int i = 0; i < countOfTrees; i++) {
+            MyTreeMap<Integer, Integer> map = new MyTreeMap<>();
+            fillTreeMap(map);
+            if (map.isBalanced()) {
+                balanced++;
+            }
+        }
+        System.out.println("Из " + countOfTrees + " MyTreeMap сбалансированы " + balanced + "\r\nнесбалансированы " + (countOfTrees - balanced) + " деревьев");
+        System.out.println("Несбалансированы " + ((((float) (countOfTrees - balanced)) * 100) / (float) countOfTrees) + " %");
+    }
+
+    private void fillTreeMap(MyTreeMap<Integer, Integer> _MyTreeMap) {
+        while (_MyTreeMap.height() < 6) {
+            _MyTreeMap.put(ThreadLocalRandom.current().nextInt(-100, 100), 0);
+        }
+    }
+}
